8a9a42a0f1b820c7acb98210146e1c4360392593
Igor Anjos <igorcatuliodosanjos@gmail.com>
2022-08-13 18:01:39 -0300

    Menu inserido: não configurado

--- src/components/PagesJSX/Home/Home.jsx
+++ src/components/PagesJSX/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Footer from "../../Footer/Footer";
+import Menu from "../../Menu/Menu";
 import Navbar from "../../Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Menu />
       <Footer />
     </div>
   );
