ae2f4f35d3c1b2af2ba26a0d63d21513992e505b
Igor Anjos <igorcatuliodosanjos@gmail.com>
2022-08-15 09:43:09 -0300

    Carrinho integrado, Funções de abrir e fechar configuradas

--- src/components/PagesJSX/Home/Home.jsx
+++ src/components/PagesJSX/Home/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 import Footer from "../../Footer/Footer";
-import Menu from "../../Menu/Menu";
 import Navbar from "../../Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Menu />
       <Footer />
     </div>
   );
