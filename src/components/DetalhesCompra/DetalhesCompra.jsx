d7355c53b3669af96941faa7bb535488ad825c1d
Igor Anjos <igorcatuliodosanjos@gmail.com>
2022-08-18 22:43:46 -0300

    funcionalidade de abrir e fechar detalhes da compra na pagina de checkout

--- src/components/DetalhesCompra/DetalhesCompra.jsx
+++ src/components/DetalhesCompra/DetalhesCompra.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ListaProdutosCheckout from "../ListaProdutosCheckout/ListaProdutosCheckout";
 
 import "./DetalhesCompra.css";
 
 function DetalhesCompra({ listaCarrinho }) {
+  const [abrirDetalhes, setAbrirDetalhes] = useState(false);
   return (
     <section className="detalhes-da-compra">
-      <div className="botao-detalhes-da-compra" id="botao-detalhes-da-compra">
+      <div
+        className="botao-detalhes-da-compra"
+        id="botao-detalhes-da-compra"
+        onClick={() => {
+          setAbrirDetalhes(!abrirDetalhes);
+        }}
+      >
         <p>Detalhes da Compra</p>
       </div>
       <ul className="lista-produtos" id="lista-produtos">
-        {listaCarrinho.map((produto) => (
-          <ListaProdutosCheckout
-            key={produto.id}
-            id={produto.id}
-            imagem={produto.imagem}
-            nome={produto.nome}
-            preco={produto.preco}
-            tamanho={produto.tamanho}
-            quantidade={produto.quantidade}
-          />
-        ))}
+        {abrirDetalhes
+          ? listaCarrinho.map((produto) => (
+              <ListaProdutosCheckout
+                key={produto.id}
+                id={produto.id}
+                imagem={produto.imagem}
+                nome={produto.nome}
+                preco={produto.preco}
+                tamanho={produto.tamanho}
+                quantidade={produto.quantidade}
+              />
+            ))
+          : ""}
       </ul>
     </section>
   );
